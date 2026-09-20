ec81df02145c5de6bd57a9d31354f4d18a685b0f
MkGold-Programmist <NewEraDeveloper@gmail.com>
2026-09-20 21:32:00 +0500

    fixed reqired files

--- auth.py
+++ auth.py
@@ -4,7 +4,7 @@ from jose import jwt
 import os
 
 # Сначала проверяет переменную окружения на Render, а если её нет (например, локально) — использует дефолтную строку
-SECRET_KEY = os.getenv("SECRET_KEY", "SUPER_SECRET_KEY_CHANGE_ME_IN_PRODUCTION")
+SECRET_KEY = os.getenv("SECRET_KEY", "SUPER_SECRET_KEY_CHANGE_ME_IN_PRODUCTION")z
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
