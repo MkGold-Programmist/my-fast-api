937f23d61f27fadcc2512bbaa7012a4fdfa954ca
MkGold-Programmist <NewEraDeveloper@gmail.com>
2026-09-21 18:19:31 +0500

    fixed main.py 1.1

--- main.py
+++ main.py
@@ -23,19 +23,12 @@ origins = [
     "http://localhost:5500",
 ]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 
 # Настройка CORS для подключения React/Vite фронтенда
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
