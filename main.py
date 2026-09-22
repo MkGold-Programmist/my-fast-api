a72b078bd1cb539f97d8b6bd7e22cf4cd3722b0b
MkGold-Programmist <NewEraDeveloper@gmail.com>
2026-09-22 16:45:02 +0500

    fixed localhosts

--- main.py
+++ main.py
@@ -47,6 +47,10 @@ app = FastAPI(
 origins = [
     "http://127.0.0.1:5500",
     "http://localhost:5500",
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "http://localhost:5175",
+    "http://localhost:5176",
 ]
 
 app.add_middleware(
