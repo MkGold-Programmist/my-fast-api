b5cb7fac5a0dede990e711a3d83e0d9fe982cf71
MkGold-Programmist <NewEraDeveloper@gmail.com>
2026-09-25 18:06:18 +0500

    allowed all responses

--- main.py
+++ main.py
@@ -44,18 +44,18 @@ app = FastAPI(
     lifespan=lifespan,
 )
 
-origins = [
-    "http://127.0.0.1:5500",
-    "http://localhost:5500",
-    "http://localhost:5173",
-    "http://localhost:5174",
-    "http://localhost:5175",
-    "http://localhost:5176",
-]
+# origins = [
+#     "http://127.0.0.1:5500",
+#     "http://localhost:5500",
+#     "http://localhost:5173",
+#     "http://localhost:5174",
+#     "http://localhost:5175",
+#     "http://localhost:5176",
+# ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
